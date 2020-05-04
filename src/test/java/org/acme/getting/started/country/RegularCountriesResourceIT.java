dee0334a4365b4a3f0839a6915e57222a5458e82
Georgios Andrianakis <geoand@gmail.com>
2020-05-04 08:13:02 +0300

    Add a native test as well

---
+++ src/test/java/org/acme/getting/started/country/RegularCountriesResourceIT.java
@@ -0,0 +1,7 @@
+package org.acme.getting.started.country;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class RegularCountriesResourceIT extends RegularCountriesResourceTest {
+}
